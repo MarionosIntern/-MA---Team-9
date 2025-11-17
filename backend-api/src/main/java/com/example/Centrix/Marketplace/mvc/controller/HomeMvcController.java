724ab00d5e6ffd39d9887903d216617091d4b0be
MarionosIntern <mdeleva888@gmail.com>
2025-11-16 21:40:16 -0500

    new frontend

--- backend-api/src/main/java/com/example/Centrix/Marketplace/mvc/controller/HomeMvcController.java
+++ backend-api/src/main/java/com/example/Centrix/Marketplace/mvc/controller/HomeMvcController.java
@@ -1,17 +1,15 @@
 package com.example.Centrix.Marketplace.mvc.controller;
 
-import com.example.Centrix.Marketplace.Provider.Provider;
-import com.example.Centrix.Marketplace.Provider.ProviderService;
-import com.example.Centrix.Marketplace.Product.Product;
-import com.example.Centrix.Marketplace.Product.ProductService;
+import java.util.List;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
-import java.util.List;
-
 import com.example.Centrix.Marketplace.Product.Product;
+import com.example.Centrix.Marketplace.Product.ProductService;
+import com.example.Centrix.Marketplace.Provider.Provider;
+import com.example.Centrix.Marketplace.Provider.ProviderService;
 
 @Controller
 public class HomeMvcController {
@@ -26,7 +24,7 @@ public class HomeMvcController {
     @GetMapping("/")
     public String home(Model model){
        List<Provider> providers =  providerService.getAll().stream().limit(5).toList();
-       List<Product> products = productService.getAllProducts().stream().limit(5).toList();
+       List<Product> products = productService.getAllProducts().stream().limit(12).toList();
        model.addAttribute("providers", providers);
        model.addAttribute("products", products);    
        return "home";
